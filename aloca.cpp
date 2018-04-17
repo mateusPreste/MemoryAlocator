3437df1257ea99678e8ad9939c7048f33c6b8a08
Mateus Oliveira Preste <mateusPreste@users.noreply.github.com>
2018-04-17 18:47:16 -0400

    Second Commit

---
+++ aloca.cpp
@@ -0,0 +1,29 @@
+//
+// Created by mateu on 17/04/2018.
+//
+
+#include "aloca.h"
+
+meualoc::meualoc(int tamanhoMemoria, int politicaMem) {
+
+}
+
+char *meualoc::aloca(unsigned short int tamanho) {
+    return nullptr;
+}
+
+char *meualoc::verifica(char *ponteiro, int posicao) {
+    return nullptr;
+}
+
+void meualoc::imprimeDados() {
+
+}
+
+int meualoc::libera(char *ponteiro) {
+    return 0;
+}
+
+meualoc::~meualoc() {
+
+}
---
+++ aloca.h
@@ -0,0 +1,40 @@
+//
+// Created by mateu on 17/04/2018.
+//
+
+#ifndef MEMORYALOCATOR_ALOCA_H
+#define MEMORYALOCATOR_ALOCA_H
+
+
+#define BESTFIT 0
+#define FIRSTFIT 1
+#define NEXTFIT 2
+
+//TAMANHO MÁXIMO DE ALOCACAO: 65535 (maior unsigned short)
+
+class meualoc{
+    char* memoria; //char* pois eh byte a byte
+public:
+    //tamanhoMemoria vai definir o tamanho da memória que o alocador vai utilizar
+    //politicaMem define como escolher o bloco de onde saira a memória
+    meualoc(int tamanhoMemoria,int politicaMem);
+
+    //Pega um pedaco da variavel memoria e aloca, retornando o ponteiro para o comeco dessa regiao e atualizando a lista de livres.
+    char *aloca(unsigned short int tamanho);
+
+    //Verifica se a posicao dada pelo ponteiro esta alocada
+    char* verifica(char* ponteiro,int posicao=0);
+
+    //Libera o espaco ocupado na posicao, de forma analoga ao free. Ou seja, liberar toda a memoria alocada para este ponteiro na funcao aloca.
+    int libera(char* ponteiro);
+
+    //Imprime o numero de elementos na lista de vazios, o maior e a media de tamanhos dos blocos vazios
+    void imprimeDados();
+
+    ~meualoc();
+
+
+};
+
+
+#endif //MEMORYALOCATOR_ALOCA_H
